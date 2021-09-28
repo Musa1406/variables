bdca91f1ed45474db6ad348dad6d58bbd365d26b
Musa1406 <91537282+Musa1406@users.noreply.github.com>
2021-09-28 15:44:17 +0300

    Initial commit

---
+++ src/variables/Main.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String studentName="Musa";
+        int studentAge=15;
+        System.out.println(studentName);
+        studentName="Musa";
+        System.out.println(studentAge);
+        System.out.println(studentName+" yaş "+studentAge);
+	}
+
+}
